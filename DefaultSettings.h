934ff54be8acc7b75a1762f4b364d5880cefe625
Oleg <69710095+DeStrikes@users.noreply.github.com>
2021-12-15 15:24:20 +0500

    Added DefaultSettings.h file

---
+++ DefaultSettings.h
@@ -0,0 +1,10 @@
+float defaultSafeZoneC = 0.5;
+float defaultMediumZoneC = 0.3;
+float defaultDangerZoneC = 0.2;
+long long defaultGenerations = 100;
+float defaultSafeFoodChance = 0.2;
+float defaulMediumFoodChance = 0.3;
+float defaultDangerFoodChance = 0.35;
+float defaultEntityGenerateChance = 0.05;
+const int defaultFieldWidth = 50;
+const int defaultFieldHeight = 50;
